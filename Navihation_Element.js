8c5b56c290ff1c5362fc07b9839f3fcf1a3be4f9
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-02-02 19:33:01 +0530

    new file:   Navihation_Element.js

---
+++ Navihation_Element.js
@@ -0,0 +1,7 @@
+document.previousElementSibling; //? Previous sibling which is an Element
+
+document.nextElementSibling; //? next sibling Element
+
+document.firstElementChild; //? first child Element
+
+document.lastElementChild; //? last child Element
