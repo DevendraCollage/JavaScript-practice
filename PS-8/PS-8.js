9338ba87affff3b89ad7a2f4b93f887f5ae0a0fe
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-02-17 21:34:18 +0530

    modified:   Exercise-3.js 	new file:   PS-8/PS-8.js 	new file:   PS-8/index.html 	new file:   jokes.css 	new file:   jokes.html

---
+++ PS-8/PS-8.js
@@ -0,0 +1,29 @@
+//* This is for google
+document.getElementById("google").addEventListener("click", function () {
+  window.open("https://google.com", "_blank");
+});
+//* This is for facebook
+document.getElementById("fb").addEventListener("click", function () {
+  window.open("https://facebook.com", "_blank");
+});
+//* This is for github
+document.getElementById("github").addEventListener("click", function () {
+  window.open("https://github.com", "_blank");
+});
+//* This is for stack over flow
+document.getElementById("sto").addEventListener("click", function () {
+  window.open("https://stackoverflow.com", "_blank");
+});
+//* This is for dev.to
+document.getElementById("dev").addEventListener("click", function () {
+  window.open("https://dev.to", "_blank");
+});
+
+//* Print the number after every 2 seconds
+// let num = 1;
+// setInterval(async function () {
+//   while (true) {
+//     console.log(await num);
+//     num++;
+//   }
+// }, 5000);
